bec646290dc4ce5b88dbe6b9e30fb95262d2d065
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2023-03-23 16:20:47 +0100

    Created interfaces

--- src/app/auth/store/actions/register.action.ts
+++ src/app/auth/store/actions/register.action.ts
@@ -1,8 +1,8 @@
 import {createAction, props} from '@ngrx/store'
-
+import {registerRequestInterface} from '../../types/registerRequest.interface'
 import {ActionTypes} from '../actionTypes'
 
 export const registerAction = createAction(
   ActionTypes.REGISTER,
-  props<{username: string; email: string; password: string}>()
+  props<registerRequestInterface>()
 )
